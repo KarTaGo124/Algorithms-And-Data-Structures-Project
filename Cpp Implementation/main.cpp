d2e939e7c669f2616bed047159af627b02c36086
Jvnc0503 <jvnc0503@gmail.com>
2025-02-22 22:28:56 -0500

    commit

--- Cpp Implementation/main.cpp
+++ Cpp Implementation/main.cpp
@@ -26,9 +26,9 @@ int main() {
     cin >> pattern;
     capitalizeString(pattern);
     if (st.search(pattern)) {
-        cout << "El patron " << pattern << " fue encontrado. \n";
+        cout << "El patron '" << pattern << "' fue encontrado. \n";
     } else {
-        cout << "El patron " << pattern << " no fue encontrado. \n";
+        cout << "El patron '" << pattern << "' no fue encontrado. \n";
     }
 
     // Algorithm 9: Find all occurrences
@@ -38,30 +38,30 @@ int main() {
     capitalizeString(substring);
     vector<int> positions = st.findAllMatches(substring);
     if (positions.empty()) {
-        cout << "El patron " << substring << " no se encontro en el texto.\n";
+        cout << "El patron '" << substring << "' no se encontro en el texto.\n";
     } else {
-        cout << "El patron " << substring << " se encontro en las posiciones: ";
+        cout << "El patron '" << substring << "' se encontro en las posiciones: ";
         for (const int pos: positions)
-            cout << pos << " ";
-        cout << endl;
+            cout << pos << ' ';
+        cout << '\n';
     }
 
     // Algorithm 10: Longest repeated substring
     const string lrs = st.longestRepeatedSubstring();
     cout << "\nLongest Repeated Substring:\n";
     if (lrs.empty()) {
-        cout << "No hay subcadenas repetidas." << endl;
+        cout << "No hay subcadenas repetidas." << '\n';
     } else {
-        cout << "La subcadena repetida mas larga es: " << lrs << endl;
+        cout << "La subcadena repetida mas larga es: " << lrs << '\n';
     }
 
     // Algorithm 11: Shortest unique substring
     const string sus = st.shortestUniqueSubstring();
     cout << "\nShortest Unique Substring:\n";
     if (sus.empty()) {
-        cout << "No hay subcadenas únicas." << endl;
+        cout << "No hay subcadenas únicas." << '\n';
     } else {
-        cout << "La subcadena unica mas corta es: " << sus << endl;
+        cout << "La subcadena unica mas corta es: " << sus << '\n';
     }
 
     return 0;
